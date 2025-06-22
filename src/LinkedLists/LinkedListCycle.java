e66f6ac99eaf5169779143b13df90b8beaea1258
Fredrik Andrén <fredrik.andren98@gmail.com>
2025-06-22 16:08:26 +0200

    Added 2 more solutions two LinkedLists

---
+++ src/LinkedLists/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package LinkedLists;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head){
+        if(head == null || head.next == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
